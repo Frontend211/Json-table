82255a88ae383be51e214852be9b24d1ba396f96
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-03-18 13:53:07 +0400

    start

---
+++ pages/static-users.js
@@ -0,0 +1,20 @@
+import Email from "../components/Email"
+import SmartList from "../components/SmartList"
+
+const api = 'https://jsonplaceholder.typicode.com/users',
+  columns = [
+    {name:'Name',getEl: obj=>obj.name},
+    {name:'Email',getEl: obj=>obj.email, comp:Email},
+    {name:'Address city',getEl: obj=>obj.address.city},
+  ];
+
+export default function StaticUsersPage({data}){
+  return <SmartList startData= {data} columns={columns} />
+}
+
+export async function getStaticProps(context) {
+  const data = await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+  return {
+    props: {data}, // will be passed to the page component as props
+  }
+}
