f0731b75a350b3caeef734390a003a3b88272d2f
Alex <user@user.com>
2023-03-25 19:27:39 +0300

    Add function Status for rick-and-morty 	modified:   components/cell-wrappers.js 	modified:   includes/api-list.js

--- components/cell-wrappers.js
+++ components/cell-wrappers.js
@@ -6,3 +6,15 @@ export function Img({value}){
   // eslint-disable-next-line @next/next/no-img-element
   return <img className="icon" src={value} alt={value}/>;
 }
+
+export function Status({ value }) {
+  //Alex
+  switch (value) {
+    case 'Alive':
+      return <span className="green">&#10003; {value}</span>;
+    case 'Dead':
+      return <span>&#8224; {value}</span>;
+    case 'unknown':
+      return <span className="red">&#63; &#63; &#63; {value}</span>;
+  }
+}
